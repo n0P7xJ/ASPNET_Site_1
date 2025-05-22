18431fdcff54df66793c79b5f78c1f9cf6d9f49d
n0P7xJ <olegp@OLEG.>
2025-05-22 17:42:25 +0300

    Initial ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ASPNET_Site_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
